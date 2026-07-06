7175bd28f22cfc1e9f4a0a0a183e93ff7a380480
Wesley Moraes <wesleymoraesserafim@gmail.com>
2026-07-06 14:42:03 -0300

    Fix content expiration: filter expired items in UI, cleanup hourly, fix banner_url bug

--- supabase/migrations/20260706000008_cleanup_hourly.sql
+++
@@ -1,8 +0,0 @@
--- Mudar cleanup de diario (3am) para a cada hora
--- Garante que conteudo expirado seja removido do DB rapidamente
-do $$ begin
-  perform cron.unschedule('cleanup-expired-content');
-exception when others then null;
-end $$;
-
-select cron.schedule('cleanup-expired-content', '0 * * * *', $$select public.cleanup_expired_content()$$);
